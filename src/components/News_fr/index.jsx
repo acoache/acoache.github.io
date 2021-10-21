9ffb0eb97c0d9bfc387eb3fa38f2b420ca227410
Anthony Coache <45765810+acoache@users.noreply.github.com>
2021-10-21 16:58:14 -0400

    Add <ul>'s for the News section

--- src/components/News_fr/index.jsx
+++ src/components/News_fr/index.jsx
@@ -6,76 +6,90 @@ function Presentations() {
   return (
     <div>
       <div className="news" id="news">
-        <li>
-          <p>
-            <b>Sept, 2021 </b>
-            – Membre du comité de programme pour l’
-            <a href="https://ai-finance.org/" target="_blank" rel="noreferrer">ACM International Conference on AI in Finance</a>
-            , qui s’est tenu en ligne du 3-5 Novembre 2021. Je faisais partie du processus
-            de d’évaluation par les pairs à double insu.
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>Juin, 2021 </b>
-            – Membre du comité organisateur du
-            <a href="https://ssc.ca/fr/congres/annuel/congres-annuel-2021/neuvieme-congres-canadien-etudiants-en-statistique" target="_blank" rel="noreferrer"> Congrès canadien des étudiants en statistique 2021</a>
-            , qui s’est tenu en ligne (plutôt qu’à l’Université Memorial à Saint-Jean de Terre-Neuve
-            en raison de la COVID-19) le 5 juin 2021. Je faisais partie de l’équipe de traduction.
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>Janv, 2021 </b>
-            – Préparé et dirigé un
-            <a href="https://github.com/acoache/matlab-bootcamp-MFI" target="_blank" rel="noreferrer"> tutoriel Matlab </a>
-            pour les étudiants du programme de maîtrise en assurance financière à l’Université de
-            Toronto.
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>Mai, 2020 </b>
-            – Membre du comité organisateur du
-            <a href="https://ssc.ca/fr/congres/annuel/congres-annuel-2020/congres-canadien-etudiants-en-statistique" target="_blank" rel="noreferrer"> Congrès canadien des étudiants en statistique 2020</a>
-            qui s’est tenu en ligne (plutôt qu’à l’Université Carleton à Ottawa en raison de la
-            COVID-19) le 30 mai 2020. Je faisais partie de l’équipe de traduction, j’étais
-            président de la session “Analyses sportives et démographiques” et modérateur.
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>Sept, 2019 </b>
-            – Découvert l’
-            <a href="http://art-aleatoire.com/" target="_blank" rel="noreferrer">art aléatoire</a>
-            , c’est-à-dire appliquer une “force stochastique” pour rendre une création
-            complètement unique.
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>Mars, 2019 </b>
-            – Co-organisé le Sommet étudiant de la statistique à Montréal qui a eu lieu le 15 mars
-            2019. Cette nouvelle initiative basée à l’UQAM visait à réunir les étudiants
-            francophones en statistique pour une journée de présentations de d’activités.
-            <a href="http://probability.ca/jeff/" target="_blank" rel="noreferrer"> Jeffrey S. Rosenthal </a>
-            et
-            <a href="https://sahirbhatnagar.com/" target="_blank" rel="noreferrer"> Sahir Rai Bhatnagar </a>
-            étaient nos deux conférenciers invités.
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>Févr, 2019 </b>
-            – Contribué au
-            <a href="https://github.com/OlivierBinette/Guide-des-bourses-UQAM" target="_blank" rel="noreferrer"> guide de soutien au développement professionnel pour les étudiants en maths-stat à l’UQAM</a>
-            un travail initié par mon ami
-            <a href="https://olivierbinette.github.io/" target="_blank" rel="noreferrer"> Olivier Binette</a>
-            Ce guide propose plusieurs exemples d’applications réussies à des
-            bourses (CVs, « statement of purpose », etc.) d’étudiants de l’UQAM
-            en plus d’offrir des liens à différentes ressources utiles.
-          </p>
-        </li>
+        <ul>
+          <li>
+            <p>
+              <b>Sept, 2021 </b>
+              – Membre du comité de programme pour l’
+              <a href="https://ai-finance.org/" target="_blank" rel="noreferrer">ACM International Conference on AI in Finance</a>
+              , qui s’est tenu en ligne du 3-5 Novembre 2021. Je faisais partie du processus
+              de d’évaluation par les pairs à double insu.
+            </p>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>
+              <b>Juin, 2021 </b>
+              – Membre du comité organisateur du
+              <a href="https://ssc.ca/fr/congres/annuel/congres-annuel-2021/neuvieme-congres-canadien-etudiants-en-statistique" target="_blank" rel="noreferrer"> Congrès canadien des étudiants en statistique 2021</a>
+              , qui s’est tenu en ligne (plutôt qu’à l’Université Memorial à Saint-Jean de Terre-Neuve
+              en raison de la COVID-19) le 5 juin 2021. Je faisais partie de l’équipe de traduction.
+            </p>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>
+              <b>Janv, 2021 </b>
+              – Préparé et dirigé un
+              <a href="https://github.com/acoache/matlab-bootcamp-MFI" target="_blank" rel="noreferrer"> tutoriel Matlab </a>
+              pour les étudiants du programme de maîtrise en assurance financière à l’Université de
+              Toronto.
+            </p>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>
+              <b>Mai, 2020 </b>
+              – Membre du comité organisateur du
+              <a href="https://ssc.ca/fr/congres/annuel/congres-annuel-2020/congres-canadien-etudiants-en-statistique" target="_blank" rel="noreferrer"> Congrès canadien des étudiants en statistique 2020</a>
+              qui s’est tenu en ligne (plutôt qu’à l’Université Carleton à Ottawa en raison de la
+              COVID-19) le 30 mai 2020. Je faisais partie de l’équipe de traduction, j’étais
+              président de la session “Analyses sportives et démographiques” et modérateur.
+            </p>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>
+              <b>Sept, 2019 </b>
+              – Découvert l’
+              <a href="http://art-aleatoire.com/" target="_blank" rel="noreferrer">art aléatoire</a>
+              , c’est-à-dire appliquer une “force stochastique” pour rendre une création
+              complètement unique.
+            </p>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>
+              <b>Mars, 2019 </b>
+              – Co-organisé le Sommet étudiant de la statistique à Montréal qui a eu lieu le 15 mars
+              2019. Cette nouvelle initiative basée à l’UQAM visait à réunir les étudiants
+              francophones en statistique pour une journée de présentations de d’activités.
+              <a href="http://probability.ca/jeff/" target="_blank" rel="noreferrer"> Jeffrey S. Rosenthal </a>
+              et
+              <a href="https://sahirbhatnagar.com/" target="_blank" rel="noreferrer"> Sahir Rai Bhatnagar </a>
+              étaient nos deux conférenciers invités.
+            </p>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>
+              <b>Févr, 2019 </b>
+              – Contribué au
+              <a href="https://github.com/OlivierBinette/Guide-des-bourses-UQAM" target="_blank" rel="noreferrer"> guide de soutien au développement professionnel pour les étudiants en maths-stat à l’UQAM</a>
+              un travail initié par mon ami
+              <a href="https://olivierbinette.github.io/" target="_blank" rel="noreferrer"> Olivier Binette</a>
+              Ce guide propose plusieurs exemples d’applications réussies à des
+              bourses (CVs, « statement of purpose », etc.) d’étudiants de l’UQAM
+              en plus d’offrir des liens à différentes ressources utiles.
+            </p>
+          </li>
+        </ul>
       </div>
     </div>
   );

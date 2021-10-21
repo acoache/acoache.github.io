9491d4d04bd4b22d5bf48e5f985772d9db398c0c
Erin Hacker <57777410+erinjhacker@users.noreply.github.com>
2021-10-21 10:34:09 -0400

    Delete website directory

--- website/src/static/bio_fr.js
+++
@@ -1,3 +0,0 @@
-const txt = 'Je suis présentement étudiant au doctorat en statistique à l’Université de Toronto (UofT) sous la supervision de Prof. Sebastian Jaimungal. J’ai obtenu un M.Sc. en mathématiques, concentration en statistique de l’Université du Québec à Montréal (UQAM) en 2019 sous la supervision de Prof. François Watier et Prof. René Ferland et j’ai gradué avec mention d’honneur du B.Sc. en mathématiques, concentration en statistique de l’UQAM en 2017. Je suis démonstrateur pour plusieurs cours de statistique et de mathématiques et j’ai également été tuteur privé pour plusieurs cours de mathématiques. Je suis aussi un assistant de recherche pour Prof. Sebastian Jaimungal, travaillant sur une collaboration entre Fields CQAM et Oanda.';
-
-export default txt;
--- website/src/static/musicDescription.js
+++
@@ -1,3 +0,0 @@
-const txt = 'Music has always been my favorite hobby. I am always on the lookout for the latest music news, in any musical style. Here are some of my favorite bands and tracks that I want to share with you, from electronic to neoclassical music. Don’t hesitate if you have any recommendations to make me!';
-
-export default txt;
